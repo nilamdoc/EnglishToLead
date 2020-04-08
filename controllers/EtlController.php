80fc9c80a2f15d8476cbd522934d99c5e99323fb
Nilam Doctor <nilamdoc@gmail.com>
2020-04-08 15:16:47 +0530

    navpall

--- controllers/EtlController.php
+++ controllers/EtlController.php
@@ -168,7 +168,6 @@ public function getOutline($_id=null){
 				'conditions' => array('outline_refer_id'=>(string)$main['_id']),
 				'order'=>array('left'=>'DESC')
 			));		
-
 	}else{
 			$outline = Outlines::find('all',array(
 				'conditions' => array('outline_refer_id'=>(string)$_id),
@@ -180,14 +179,12 @@ public function getOutline($_id=null){
 		$allnext = array();
 			
 			foreach($outline as $o){
-//				print_r($o['_id']."**".$o['outline_name']."--".$o['outline_refer_id']."@@@@@@@@@@@@@@@\n");
+//				print_r($o['_id']."**".$o['outline_name']."--".$o['_id']."@@@@@@@@@@@@@@@\n");
 				$depth = Outlines::find('first',array(
 					'conditions'=>array('outline_refer_id'=>(string)$o['_id']),
 				));
 				array_push($allnext,$depth);
 			}
-
-	
 	return $this->render(array('json' => array("success"=>"Yes",'outline'=>$outline, 'down'=>$downLevels, 'countDown'=> count($downLevels), 'main'=>$main,'allnext'=>$allnext)));		
 }
 
